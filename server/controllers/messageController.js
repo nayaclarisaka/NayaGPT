8b5c15e35004fb3d14e6168c7bb56523616dcc0c
Naya <clariqueen8@gmail.com>
2026-08-26 13:23:17 +0700

    update perubahan

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -20,7 +20,7 @@ export const textMessageController = async (req, res) => {
         chat.messages.push({ role: "user", content: prompt, timestamp: Date.now(), isImage: false })
 
         const { choices } = await openai.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-3.5-flash",
             messages: [
                 {
                     role: "user",
